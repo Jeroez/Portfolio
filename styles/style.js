ca730e1fff8afb7f71bbd273d65b3ff2118dc320
Jeremiah <jeremiahbnjabgmz@gmail.com>
2024-01-26 00:30:58 +0800

    update

---
+++ styles/style.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navigation = document.getElementById('navigation'); 
+    var homeSection = document.getElementById('Home');
+
+    window.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY;
+        var homeSectionHeight = homeSection.offsetHeight;
+
+        if (scrollPosition < homeSectionHeight * 1.0) {
+            navigation.style.display = 'block';
+        } else {
+            navigation.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar= document.getElementById('navbar');
+    var homeSection = document.getElementById('Home');
+
+    window.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY;
+        var homeSectionHeight = homeSection.offsetHeight;
+
+        if (scrollPosition > homeSectionHeight * 1.0) {
+            navbar.style.display = 'block';
+        } else {
+            navbar.style.display = 'none';
+        }
+    });
+});
